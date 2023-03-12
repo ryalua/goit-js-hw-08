07ce3ad243bb35b0ada10fe7d2c94d227592b13e
Alex <alexrybalka65@gmail.com>
2023-03-12 18:27:53 +0200

    home-work 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    galleryCreature: document.querySelector('.gallery'),
+};
+
+const makeGallery = ({ preview, original, description,
+}) => `<li>
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+            </a>
+        </li>`;
+
+const markup = galleryItems
+  .map((image) => makeGallery(image) )
+  .join('');
+  
+refs.galleryCreature.insertAdjacentHTML("afterbegin", markup);
+
+new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+        scrollZoom: true,
+        scrollZoomFactor: 0.1,
+});
