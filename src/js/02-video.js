a868aa005b4522974158b61dac22f432ae0301da
Alex <alexrybalka65@gmail.com>
2023-03-13 14:34:03 +0200

    done 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const TIME_PLAIED_KEY = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(timeSeved, 1000));
+
+function timeSeved(event) {
+    let secondsPlaied = event.seconds;
+    localStorage.setItem(TIME_PLAIED_KEY, secondsPlaied);
+}   
+const plaiedCurrentTime = localStorage.getItem(TIME_PLAIED_KEY);
+
+if (plaiedCurrentTime) {
+    player.setCurrentTime(plaiedCurrentTime);
+}
+ 
