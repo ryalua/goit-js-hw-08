7b48cf2669a4d27bd2f6f9ad872d67dacec7658b
Alex <alexrybalka65@gmail.com>
2023-03-14 14:57:10 +0200

    done 03-feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,7 +11,7 @@ player.on('timeupdate', throttle(timeSeved, 1000));
 function timeSeved(event) {
     let secondsPlaied = event.seconds;
     localStorage.setItem(TIME_PLAIED_KEY, secondsPlaied);
-}   
+};  
 const plaiedCurrentTime = localStorage.getItem(TIME_PLAIED_KEY);
 
 if (plaiedCurrentTime) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from "lodash.throttle";
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+};
+refs.form.addEventListener('submit', handleFormSubmit);
+refs.form.addEventListener('input', throttle(handleFormInput, 500));
+
+let formInput = {};
+const STORAGE_KEY = "feedback-form-state";
+storageState();
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formInput);
+};
+
+function handleFormInput(evt) {
+    formInput = {
+        email: refs.email.value,
+        message: refs.message.value,
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formInput));
+};
+    
+function storageState() {
+    const currentState = localStorage.getItem(STORAGE_KEY);
+    if (currentState) {
+        formInput = JSON.parse(currentState);
+        refs.email.value = formInput.email;
+        refs.message.value = formInput.message;
+    }
+}
+
+
 
